b450d9046a53354c600fa1716b667724b65d4346
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-11-19 16:53:56 +0000

    Updated PathwayGenie, making some progress with progress.

--- run_pathwaygenie.py
+++ run_pathwaygenie.py
@@ -4,7 +4,8 @@ Created on 19 Nov 2015
 @author: neilswainston
 '''
 import ast
-from flask import Flask, render_template, request
+import time
+from flask import Flask, Response, render_template, request, session
 from rbs import RBSSolution
 from synbiochemdev.optimisation import simulated_annealing as sim_ann
 
@@ -28,9 +29,30 @@ def submit():
     taxonomy_id = '83333'
     len_target = int(request.form['len_target'])
     tir_target = float(request.form['tir_target'])
+
+    session['in_progress'] = True
+    return render_template('submitted.html')
+
+    # Do job in new thread, return result when completed:
     sim_ann.optimise(RBSSolution(protein_ids, taxonomy_id, len_target,
                                  tir_target), verbose=True)
-    return render_template('submitted.html')
+    return render_template('finished.html', result=None)
+
+
+@app.route('/progress')
+def get_progress():
+
+    in_progress = True
+
+    def check_progress():
+        x = 0
+        while in_progress:
+            print x
+            x = (x + 10) % 100
+            time.sleep(1)
+            yield "data:" + str(x) + "\n\n"
+
+    return Response(check_progress(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
