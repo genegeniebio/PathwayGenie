7e9305bfc7f1cb198c2c7adeabdb0de2b9acba31
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-12-17 18:59:28 +0100

    Interim check-in of PathwayGenie.

--- static/utils.js
+++ static/utils.js
@@ -22,3 +22,14 @@ function getJSON(form)
 	
 	return JSON.stringify(json);
 }
+
+
+function guid()
+{
+	function s4()
+	{
+		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	}
+	
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
