e1c420932c15a4494e9789fa089f449dd9147f03
Neil Swainston <neil.swainston@manchester.ac.uk>
2016-09-07 10:49:39 +0100

    Replaced duplicate code with sequence_utils.get_seq_by_melt_temp.

--- dominogenie.py
+++ dominogenie.py
@@ -7,9 +7,8 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
 '''
-
-from synbiochem.utils import sequence_utils
 import synbiochem.utils
+import synbiochem.utils.sequence_utils as seq_utils
 
 
 def get_dominoes(target_melt_temp, sequences, reagent_concs=None):
@@ -20,19 +19,7 @@ def get_dominoes(target_melt_temp, sequences, reagent_concs=None):
 
 def _get_domino(pair, target_melt_temp, reagent_concs=None):
     '''Get bridging oligo for pair of sequences.'''
-    return (_get_domino_part(pair[0], False, target_melt_temp, reagent_concs),
-            _get_domino_part(pair[1], True, target_melt_temp, reagent_concs))
-
-
-def _get_domino_part(sequence, forward, target_melt_temp, reagent_concs=None):
-    # TODO: replace with sequence_utils.get_seq_by_melt_temp
-    '''Gets half of bridging oligo.'''
-    for i in range(1, len(sequence)):
-        subsequence = sequence[:(i + 1)] if forward else sequence[-(i + 1):]
-        melting_temp = sequence_utils.get_melting_temp(subsequence, None,
-                                                       reagent_concs)
-
-        if melting_temp > target_melt_temp:
-            return subsequence, melting_temp
-
-    return None, -float('inf')
+    return (seq_utils.get_seq_by_melt_temp(pair[0], target_melt_temp, False,
+                                           reagent_concs),
+            seq_utils.get_seq_by_melt_temp(pair[1], target_melt_temp,
+                                           reagent_concs))
