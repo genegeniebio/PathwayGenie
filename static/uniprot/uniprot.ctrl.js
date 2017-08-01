5e43ddccaef31399eb27230db83592a58e8a63df
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-08-01 16:19:48 +0100

    Added selection.

--- static/uniprot/uniprot.ctrl.js
+++ static/uniprot/uniprot.ctrl.js
@@ -1,6 +1,16 @@
 uniprotApp.controller("uniprotCtrl", ["$uibModalInstance", "options", function($uibModalInstance, options) {
 	var self = this;
 	self.options = options;
+	self.selected = null;
+	
+	self.toggleSelected = function(selected) {
+		if(self.selected === selected) {
+			self.selected = null;
+		}
+		else {
+			self.selected = selected;
+		}
+	};
 	
 	self.cancel = function() {
 		$uibModalInstance.close();
