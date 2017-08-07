1c98dd847995f5f7b014fc361299dde75b8fcca4
Neil Swainston <neil.swainston@manchester.ac.uk>
2017-08-07 20:13:26 +0100

    Fixed form validity watch.

--- static/design/feature.directive.js
+++ static/design/feature.directive.js
@@ -8,8 +8,8 @@ designApp.directive("featurePanel", function() {
     	},
         templateUrl: "/static/design/feature.html",
         link: function (scope, element, attr, ctrl) {
-            scope.$watch("form.$valid", function() {
-            	scope.setValid({valid: scope.form.$valid});
+            scope.$watch("form.$valid", function(newVal) {
+            	scope.setValid({valid: newVal});
             });
         }
     };
