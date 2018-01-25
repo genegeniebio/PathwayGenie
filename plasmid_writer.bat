bb576c0319b78df9efa8a0915eca52dc75786a50
Neil Swainston <neil.swainston@manchester.ac.uk>
2018-01-25 13:05:25 +0000

    Updated plasmid_writer.bat.

--- plasmid_writer.bat
+++ plasmid_writer.bat
@@ -1,13 +1,8 @@
-pip install pandas
-pip install synbiochem-py
+c:\Python27\Scripts\pip install pandas
+c:\Python27\Scripts\pip install synbiochem-py
 
 set PYTHONPATH=%PYTHONPATH%;.
 
-python \
-	scripts/plasmid_writer.py \
-	scripts/in.csv \
-	scripts/out.csv \
-	https://ice.synbiochem.co.uk \
-	USERNAME \
-	PASSWORD \
-	synbiochem \
+python scripts/plasmid_writer.py scripts/in.csv scripts/out.csv https://ice.synbiochem.co.uk USERNAME PASSWORD synbiochem
+
+pause
