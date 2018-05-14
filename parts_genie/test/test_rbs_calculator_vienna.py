d9ff9417b21af173da12568235379f4e79b96a43
neilswainston <neil.swainston@manchester.ac.uk>
2018-05-14 14:45:08 +0100

    Added specific tests for NUPACK and Vienna.

---
+++ parts_genie/test/test_rbs_calculator_vienna.py
@@ -0,0 +1,52 @@
+'''
+PathwayGenie (c) GeneGenie Bioinformatics Ltd. 2018
+
+PathwayGenie is licensed under the MIT License.
+
+To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
+
+@author:  neilswainston
+'''
+import unittest
+
+from parts_genie.rbs_calculator import RbsCalculator
+import parts_genie.vienna_utils as utils
+
+
+class TestRbsCalculator(unittest.TestCase):
+    '''Test class for RbsCalculator.'''
+
+    def test_calc_kinetic_score(self):
+        '''Tests calc_kinetic_score method.'''
+        r_rna = 'acctcctta'
+        calc = RbsCalculator(r_rna, utils)
+
+        m_rna = 'TTCTAGAGGGGGGATCTCCCCCCAAAAAATAAGAGGTACACATGACTAAAACTTTCA' + \
+            'AAGGCTCAGTATTCCCACTGAG'
+
+        start_pos = 41
+        self.assertAlmostEqual(calc.calc_kinetic_score(m_rna, start_pos),
+                               0.528571428571428)
+
+    def test_get_calc_dgs(self):
+        '''Tests calc_dgs method.'''
+        r_rna = 'acctcctta'
+        calc = RbsCalculator(r_rna, utils)
+
+        m_rna = 'TTCTAGAGGGGGGATCTCCCCCCAAAAAATAAGAGGTACACATGACTAAAACTTTCA' + \
+            'AAGGCTCAGTATTCCCACTGAG'
+
+        dgs = calc.calc_dgs(m_rna)
+        self.assertEqual(dgs.keys(), [41, 74])
+        self.assertAlmostEqual(dgs[41][0], -6.088674036389431)
+        self.assertAlmostEqual(dgs[74][0], 5.793940143051147)
+
+        dgs = calc.calc_dgs(m_rna)
+        self.assertEqual(dgs.keys(), [41, 74])
+        self.assertAlmostEqual(dgs[41][0], -6.088674036389431)
+        self.assertAlmostEqual(dgs[74][0], 5.793940143051147)
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
