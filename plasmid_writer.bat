342cc7b4dbc0590a7d776ec486e649885e344bd8
neilswainston <neil.swainston@manchester.ac.uk>
2019-04-30 13:51:06 +0100

    Updated plasmid and strain write code.

--- plasmid_writer.bat
+++ plasmid_writer.bat
@@ -3,6 +3,6 @@ c:\Python27\Scripts\pip install synbiochem-py
 
 set PYTHONPATH=%PYTHONPATH%;.
 
-python scripts/plasmid_writer.py scripts/in.csv scripts/out.csv https://ice.synbiochem.co.uk USERNAME PASSWORD synbiochem
+python scripts/plasmid_writer.py scripts/plasmid_writer_in.csv scripts/plasmid_writer_out.csv https://ice.synbiochem.co.uk USERNAME PASSWORD synbiochem
 
 pause
--- strain_writer.sh
+++ strain_writer.sh
@@ -4,9 +4,9 @@ pip install synbiochem-py
 export PYTHONPATH=$PYTHONPATH:.
 
 python \
-	scripts/plasmid_writer.py \
-	scripts/plasmid_writer_in.csv \
-	scripts/plasmid_writer_out.csv \
+	scripts/strain_writer.py \
+	scripts/strain_writer_in.csv \
+	scripts/strain_writer_out.csv \
 	https://ice.synbiochem.co.uk \
 	USERNAME \
 	PASSWORD \
