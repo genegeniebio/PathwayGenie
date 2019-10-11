3a02e915568b8d4e66eb606dad444d787b4f6437
neilswainston <neilswainston@10.24.57.56>
2019-10-11 15:21:13 +0100

    Added support for client module.

--- pathway_genie/sbol_utils.py
+++ pathway_genie/sbol_utils.py
@@ -1,4 +1,4 @@
-"""
+'''
 PathwayGenie (c) GeneGenie Bioinformatics Ltd. 2018
 
 PathwayGenie is licensed under the MIT License.
@@ -6,25 +6,24 @@ PathwayGenie is licensed under the MIT License.
 To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 
 @author:  neilswainston
-"""
-from collections import defaultdict
+'''
 import sys
 
 from sbol import Document
 from synbiochem.utils.seq_utils import get_uniprot_values
 
-_CDS_SO = "http://identifiers.org/so/SO:0000316"
+_CDS_SO = 'http://identifiers.org/so/SO:0000316'
 
 
 def to_query(filenames):
     '''Convert SBOL documents to PartsGenie query.'''
     docs = _parse(filenames)
     uniprot_docs = _get_uniprot_docs(docs)
-    return _to_query(uniprot_docs.keys())
+    return _to_query(uniprot_docs)
 
 
 def _parse(filenames):
-    """Parse SBOL documents."""
+    '''Parse SBOL documents.'''
     docs = [Document() for _ in filenames]
 
     for doc, filename in zip(docs, filenames):
@@ -34,122 +33,122 @@ def _parse(filenames):
 
 
 def _get_uniprot_docs(docs):
-    """Get uniprot docs."""
-    uniprot_docs = defaultdict(dict)
+    '''Get uniprot docs.'''
+    uniprot_docs = {}
 
-    for doc_idx, doc in enumerate(docs):
+    for doc in docs:
         for comp_def in doc.componentDefinitions:
             if _CDS_SO in comp_def.roles:
-                uniprot_docs[comp_def.description][doc_idx] = comp_def.identity
+                uniprot_docs[comp_def.description] = comp_def.identity
 
     return uniprot_docs
 
 
-def _to_query(uniprot_ids):
-    """Get query."""
+def _to_query(uniprot_docs):
+    '''Get query.'''
     query = {}
-    query["app"] = "PartsGenie"
+    query['app'] = 'PartsGenie'
 
-    query["designs"] = []
+    query['designs'] = []
 
-    for uniprot_id in uniprot_ids:
+    for uniprot_id, sbol_comp_def in uniprot_docs.items():
         try:
-            query["designs"].append(_get_design(uniprot_id))
+            query['designs'].append(_get_design(uniprot_id, sbol_comp_def))
         except ValueError:
             pass
 
-    query["filters"] = {
-        "max_repeats": 5,
-        "gc_min": 0.25,
-        "gc_max": 0.65,
-        "local_gc_window": 50,
-        "local_gc_min": 0.15,
-        "local_gc_max": 0.8,
-        "restr_enzs": [],
-        "excl_codons": []
+    query['filters'] = {
+        'max_repeats': 5,
+        'gc_min': 0.25,
+        'gc_max': 0.65,
+        'local_gc_window': 50,
+        'local_gc_min': 0.15,
+        'local_gc_max': 0.8,
+        'restr_enzs': [],
+        'excl_codons': []
     }
 
-    query["organism"] = {
-        "taxonomy_id": "37762",
-        "name": "Escherichia coli",
-        "r_rna": "acctccttt"
+    query['organism'] = {
+        'taxonomy_id': '37762',
+        'name': 'Escherichia coli',
+        'r_rna': 'acctccttt'
     }
 
     return query
 
 
-def _get_design(uniprot_id):
-    """Get design."""
+def _get_design(uniprot_id, sbol_comp_def):
+    '''Get design.'''
     design = {}
-    design["name"] = "Part"
-    design["desc"] = "Part"
+    design['name'] = 'Part'
+    design['desc'] = sbol_comp_def
 
     # Flanking region:
     flank = {
-        "typ": "http://purl.obolibrary.org/obo/SO_0000143",
-        "name": "Sequence of defined melting temperature",
-        "seq": "",
-        "parameters": {
-            "Tm target": 70
+        'typ': 'http://purl.obolibrary.org/obo/SO_0000143',
+        'name': 'Sequence of defined melting temperature',
+        'seq': '',
+        'parameters': {
+            'Tm target': 70
         },
-        "temp_params": {
-            "fixed": True,
-            "required": ["name", "tm"],
-            "valid": True,
-            "id": "_flank_id"
+        'temp_params': {
+            'fixed': True,
+            'required': ['name', 'tm'],
+            'valid': True,
+            'id': '_flank_id'
         }
     }
 
     # RBS:
     rbs = {
-        "typ": "http://purl.obolibrary.org/obo/SO_0000139",
-        "name": "RBS",
-        "end": 60,
-        "parameters": {
-            "TIR target": 15000
+        'typ': 'http://purl.obolibrary.org/obo/SO_0000139',
+        'name': 'RBS',
+        'end': 60,
+        'parameters': {
+            'TIR target': 15000
         },
-        "temp_params": {
-            "fixed": False,
-            "required": ["name", "tir"],
-            "min_end": 35,
-            "max_end": 10000,
-            "valid": True,
-            "id": "_rbs_id"
+        'temp_params': {
+            'fixed': False,
+            'required': ['name', 'tir'],
+            'min_end': 35,
+            'max_end': 10000,
+            'valid': True,
+            'id': '_rbs_id'
         }
     }
 
     # CDS:
-    uniprot_vals = get_uniprot_values([uniprot_id], ["sequence"])
+    uniprot_vals = get_uniprot_values([uniprot_id], ['sequence'])
 
     if uniprot_id not in uniprot_vals:
-        raise ValueError("Uniprot id not found: %s" % uniprot_id)
+        raise ValueError('Uniprot id not found: %s' % uniprot_id)
 
     cds = {
-        "typ": "http://purl.obolibrary.org/obo/SO_0000316",
-        "name": uniprot_id,
-        "temp_params": {
-            "fixed": False,
-            "required": ["name", "prot"],
-            "valid": True,
-            "id": "_cds_id",
-            "aa_seq": uniprot_vals[uniprot_id]["Sequence"],
-            "orig_seq": uniprot_vals[uniprot_id]["Sequence"]
+        'typ': 'http://purl.obolibrary.org/obo/SO_0000316',
+        'name': uniprot_id,
+        'temp_params': {
+            'fixed': False,
+            'required': ['name', 'prot'],
+            'valid': True,
+            'id': '_cds_id',
+            'aa_seq': uniprot_vals[uniprot_id]['Sequence'],
+            'orig_seq': uniprot_vals[uniprot_id]['Sequence']
         },
-        "desc": "",
-        "links": ["http://identifiers.org/uniprot/%s" % uniprot_id]
+        'desc': '',
+        'links': ['http://identifiers.org/uniprot/%s' % uniprot_id]
     }
 
-    design["features"] = [flank, rbs, cds, flank]
+    design['features'] = [flank, rbs, cds, flank]
 
     return design
 
 
 def main(args):
-    """main method."""
+    '''main method.'''
     import json
     query = to_query(args)
     print(json.dumps(query, indent=4))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
