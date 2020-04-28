afa54d6574ed54e152c4a633c29051a29fc42703
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-04-28 22:54:08 +0100

    Removed obsolete code.

--- ice/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-PathwayGenie (c) GeneGenie Bioinformatics Ltd. 2018
-
-PathwayGenie is licensed under the MIT License.
-
-To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
-
-@author:  neilswainston
-'''
